be1c461fd83f809a9058fcd307b7f9a3077a84bc
Milkessa-Oljira <milkessaoljira@gmail.com>
2021-12-16 20:07:06 +0300

    merged changes

--- lib/ui/auth/change_password.dart
+++ lib/ui/auth/change_password.dart
@@ -16,7 +16,7 @@ class _ChangePasswordState extends State<ChangePassword> {
   TextEditingController _oldPasswordController = new TextEditingController();
   TextEditingController _newPasswordController = new TextEditingController();
   TextEditingController _confirmPasswordController =
-  new TextEditingController();
+      new TextEditingController();
   bool _status = true;
   @override
   Widget build(BuildContext context) {
@@ -52,7 +52,7 @@ class _ChangePasswordState extends State<ChangePassword> {
                                 ),
                                 Padding(
                                   padding:
-                                  EdgeInsets.only(left: 25.0, top: 15.0),
+                                      EdgeInsets.only(left: 25.0, top: 15.0),
                                   child: new Text('Change Password',
                                       style: TextStyle(
                                           fontWeight: FontWeight.bold,
@@ -230,48 +230,48 @@ class _ChangePasswordState extends State<ChangePassword> {
               padding: EdgeInsets.only(right: 10.0),
               child: Container(
                   child: new RaisedButton(
-                    child: new Text("Save"),
-                    textColor: Colors.white,
-                    color: Colors.green,
-                    onPressed: () async {
-                      _formKey.currentState.save();
-                      print("object ${_formKey.currentState.validate()}");
-                      if (_formKey.currentState.validate()) {
-                        if (_newPasswordController.text !=
-                            _confirmPasswordController.text) {
-                          showTopSnackBar(
-                            context,
-                            CustomSnackBar.error(
-                              message: "Please enter the same password",
-                            ),
-                          );
-                        } else {
-                          var res = await auth.updatePassword(
-                              _newPasswordController.text,
-                              _oldPasswordController.text);
-                          print("res $res");
-                          if (res['status']) {
-                            showTopSnackBar(
-                              context,
-                              CustomSnackBar.success(
-                                message: "Your password is changed successfully",
-                              ),
-                            );
-                          } else if (res['message'] == 302) {
-                            showTopSnackBar(
-                              context,
-                              CustomSnackBar.error(
-                                message:
+                child: new Text("Save"),
+                textColor: Colors.white,
+                color: Colors.green,
+                onPressed: () async {
+                  _formKey.currentState.save();
+                  print("object ${_formKey.currentState.validate()}");
+                  if (_formKey.currentState.validate()) {
+                    if (_newPasswordController.text !=
+                        _confirmPasswordController.text) {
+                      showTopSnackBar(
+                        context,
+                        CustomSnackBar.error(
+                          message: "Please enter the same password",
+                        ),
+                      );
+                    } else {
+                      var res = await auth.updatePassword(
+                          _newPasswordController.text,
+                          _oldPasswordController.text);
+                      print("res $res");
+                      if (res['status']) {
+                        showTopSnackBar(
+                          context,
+                          CustomSnackBar.success(
+                            message: "Your password is changed successfully",
+                          ),
+                        );
+                      } else if (res['message'] == 302) {
+                        showTopSnackBar(
+                          context,
+                          CustomSnackBar.error(
+                            message:
                                 "Please make sure you entered the correct old password",
-                              ),
-                            );
-                          }
-                        }
+                          ),
+                        );
                       }
-                    },
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0)),
-                  )),
+                    }
+                  }
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
             ),
             flex: 2,
           ),
@@ -280,18 +280,18 @@ class _ChangePasswordState extends State<ChangePassword> {
               padding: EdgeInsets.only(left: 10.0),
               child: Container(
                   child: new RaisedButton(
-                    child: new Text("Cancel"),
-                    textColor: Colors.white,
-                    color: Colors.red,
-                    onPressed: () {
-                      setState(() {
-                        _status = true;
-                        FocusScope.of(context).requestFocus(new FocusNode());
-                      });
-                    },
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0)),
-                  )),
+                child: new Text("Cancel"),
+                textColor: Colors.white,
+                color: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    _status = true;
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                  });
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
             ),
             flex: 2,
           ),
--- lib/ui/pharmacy/ui/prescription/result_phone.dart
+++ lib/ui/pharmacy/ui/prescription/result_phone.dart
@@ -29,13 +29,13 @@ class _PrescriptionResultState extends State<PrescriptionResultPhone> {
 
     List<dynamic> result = widget.result;
     var patient = result[0];
-    var prescription=result[0]['prescription'];
-    List<dynamic> notReadPrescription = prescription.where((i) => i['status']=="NotRead").toList();
-    List<dynamic> list_id=[];
+    var prescription = result[0]['prescription_item'];
+    List<dynamic> notReadPrescription =
+        prescription.where((i) => i['status'] == "NotRead").toList();
+    List<dynamic> list_id = [];
     notReadPrescription.forEach((element) {
       list_id.add(element['id']);
     });
-    print("readreadread $list_id");
     return SafeArea(
       child: Scaffold(
         body: Column(
@@ -46,175 +46,186 @@ class _PrescriptionResultState extends State<PrescriptionResultPhone> {
             ),
             Expanded(
                 child: ListView(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 650.0,
-                        decoration: BoxDecoration(
-                            border:
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 650.0,
+                    decoration: BoxDecoration(
+                        border:
                             Border.all(width: 2.0, color: Color(0xff707070))),
-                        child: ListView(
-                        shrinkWrap: true,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'RX',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 23.0),
-                              ),
-                            ),
-                            Column(
-                                children: notReadPrescription.map<Widget>((e) {
-                                  return Row(
-                                    children: [
-                                      Text(
-                                        '${notReadPrescription.indexOf(e) + 1}',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20.0),
-                                      ),
-                                      SizedBox(
-                                        width: 5.0,
-                                      ),
-                                      Expanded(
-                                        child: Text(
-                                          ' ${e['drug_name']} ${e['strength']} '
-                                              '${e['unit']} ${e['route']} Every ${e['frequency']} For ${e['takein']}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20.0),
-                                        ),
-                                      ),
-                                      InkWell(
-                                        child: Container(
-                                          width: 20,
-                                          height: 20,
-                                          margin:EdgeInsets.only(right: 10.0),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  color: Colors.black26, width: 1.5)),
-                                          child: selectedList.contains(e['id'])
-                                              ? FittedBox(
-                                              child: Icon(
-                                                Icons.cancel,
-                                                size: 10,
-                                              ))
-                                              : Container(),
-                                        ),
-                                        onTap: () {
-                                          setState(() {
-                                            var selected =
-                                            selectedList.contains(e['id']);
-                                            if (!selected) {
-                                              selectedList.add(e['id']);
-                                            } else {
-                                              selectedList.remove(e['id']);
-                                            }
-                                          });
-
-                                        },
-                                      ),
-                                      // Checkbox(
-                                      //   value: selectedList.contains(e['id']),
-                                      //
-                                      //   onChanged: (bool value) {
-                                      //     setState(() {
-                                      //       if (value) {
-                                      //         selectedList.add(e['id']);
-                                      //       } else {
-                                      //         selectedList.remove(e['id']);
-                                      //       }
-                                      //     });
-                                      //   },
-                                      //
-                                      // ),
-                                    ],
-                                  );
-                                }).toList())
-                          ],
-                        ),
-                      ),
-                    ),
-                    prescProvider.sentStatus == PrescriptionStatus.Sending
-                        ? loading
-                        : Row(
+                    child: ListView(
+                      shrinkWrap: true,
                       children: [
-                        SizedBox(
-                          width: 15.0,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Container(
-                              width: 100,
-                              height: 40,
-                              margin: EdgeInsets.only(right: 20.0, top: 0.0),
-                              decoration: BoxDecoration(
-                                  color: Color(0xff88DE91),
-                                  border: Border.all(color: Colors.black45),
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              child: Center(
-                                  child: Text(
-                                    'Cancel',
-                                    style: TextStyle(
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold),
-                                  )),
-                            ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'RX',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 23.0),
                           ),
                         ),
-                        GestureDetector(
-                          onTap: () async {
-                            var res = await prescProvider
-                                .acceptPrescription(selectedList,list_id);
-                            if (res['status']) {
-                              showTopSnackBar(
-                                context,
-                                CustomSnackBar.success(
-                                  message:
-                                  'Your have accepted prescriptions successfully',
+                        Column(
+                            children: notReadPrescription.map<Widget>((e) {
+                          return Row(
+                            children: [
+                              Text(
+                                '${notReadPrescription.indexOf(e) + 1}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0),
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  ' ${e['drug_name']} ${e['strength']} '
+                                  '${e['unit']} ${e['route']} Every ${e['frequency']} For ${e['takein']}',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0),
                                 ),
-                              );
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=>WelcomePharmacy()));
-                            } else {
-                              showTopSnackBar(
-                                context,
-                                CustomSnackBar.error(
-                                    message:
-                                    'Unable to accept prescriptions'
+                              ),
+                              InkWell(
+                                child: Container(
+                                  width: 20,
+                                  height: 20,
+                                  margin: EdgeInsets.only(right: 10.0),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black26, width: 1.5)),
+                                  child: selectedList.contains(e['id'])
+                                      ? FittedBox(
+                                          child: Icon(
+                                          Icons.cancel,
+                                          size: 10,
+                                        ))
+                                      : Container(),
                                 ),
-                              );
-                            }
-                          },
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Container(
-                              width: 100,
-                              height: 40,
-                              margin: EdgeInsets.only(right: 20.0, top: 0.0),
-                              decoration: BoxDecoration(
-                                  color: Color(0xff07febb),
-                                  border: Border.all(color: Colors.black45),
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              child: Center(
-                                  child: Text(
-                                    'Accept',
-                                    style: TextStyle(
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold),
-                                  )),
-                            ),
-                          ),
-                        )
+                                onTap: () {
+                                  setState(() {
+                                    var selected =
+                                        selectedList.contains(e['id']);
+                                    if (!selected) {
+                                      selectedList.add(e['id']);
+                                    } else {
+                                      selectedList.remove(e['id']);
+                                    }
+                                  });
+                                },
+                              ),
+                              // Checkbox(
+                              //   value: selectedList.contains(e['id']),
+                              //
+                              //   onChanged: (bool value) {
+                              //     setState(() {
+                              //       if (value) {
+                              //         selectedList.add(e['id']);
+                              //       } else {
+                              //         selectedList.remove(e['id']);
+                              //       }
+                              //     });
+                              //   },
+                              //
+                              // ),
+                            ],
+                          );
+                        }).toList())
                       ],
                     ),
-                  ],
-                ))
+                  ),
+                ),
+                prescProvider.sentStatus == PrescriptionStatus.Sending
+                    ? loading
+                    : Row(
+                        children: [
+                          SizedBox(
+                            width: 15.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Container(
+                                width: 100,
+                                height: 40,
+                                margin: EdgeInsets.only(right: 20.0, top: 0.0),
+                                decoration: BoxDecoration(
+                                    color: Color(0xff88DE91),
+                                    border: Border.all(color: Colors.black45),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Center(
+                                    child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              if (selectedList.length == 0) {
+                                showTopSnackBar(
+                                  context,
+                                  CustomSnackBar.error(
+                                      message:
+                                          'Please select at least one prescription'),
+                                );
+                              } else {
+                                var res = await prescProvider
+                                    .acceptPrescription(selectedList, list_id);
+                                if (res['status']) {
+                                  showTopSnackBar(
+                                    context,
+                                    CustomSnackBar.success(
+                                      message:
+                                          'Your have accepted prescriptions successfully',
+                                    ),
+                                  );
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              WelcomePharmacy()));
+                                } else {
+                                  showTopSnackBar(
+                                    context,
+                                    CustomSnackBar.error(
+                                        message:
+                                            'Unable to accept prescriptions'),
+                                  );
+                                }
+                              }
+                            },
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Container(
+                                width: 100,
+                                height: 40,
+                                margin: EdgeInsets.only(right: 20.0, top: 0.0),
+                                decoration: BoxDecoration(
+                                    color: Color(0xff07febb),
+                                    border: Border.all(color: Colors.black45),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Center(
+                                    child: Text(
+                                  'Accept',
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+              ],
+            ))
           ],
         ),
       ),
